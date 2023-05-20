6cff53a33ea9494647fa6b392c203e72ae176f9d
shunsuke-tamura <wwwxx4869@gmail.com>
2023-05-21 07:04:55 +0900

    update: hotreload対応

--- bin/dart_shelf_rest_api.dart
+++ bin/dart_shelf_rest_api.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:dart_shelf_rest_api/product_list.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf/shelf.dart';
+import 'package:shelf_hotreload/shelf_hotreload.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 Response _healthcheckHandler(request) {
   return Response.ok(
-    const JsonEncoder.withIndent('  ').convert({'greeting': 'OK'}),
+    const JsonEncoder.withIndent('  ').convert({'greeting': 'OKK'}),
     headers: {'content-type': 'application/json'},
   );
 }
@@ -23,8 +25,15 @@ class Service {
   }
 }
 
-void main() async {
+Future<HttpServer> createServer() async {
   final service = Service();
   final server = await shelf_io.serve(service.handler, 'localhost', 8081);
   print('Server running on localhost:${server.port}');
+  return server;
+}
+
+// Run this app with --enable-vm-service (or use debug run)
+// dart run --enable-vm-service bin/dart_shelf_rest_api.dart
+void main() async {
+  withHotreload(() => createServer());
 }
